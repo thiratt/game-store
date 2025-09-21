b7534cea792aa4555795b0ad4e0de178ba0d33b8
thiratt <thiratcha7@gmail.com>
2025-09-21 13:35:59 +0700

    First commit: with Tailwind CSS and Material UI libraries

---
+++ README.md
@@ -0,0 +1,8 @@
+# Game Store 🐾🎮
+
+Welcome to **Game Store** — where noobs like me fumble through Stone or Episode (iykyk). This project serves as a dedicated space for experimentation, learning, and the occasional unexpected discovery.
+
+---
+
+> 🚧 **Work in Progress!**  
+> This project is currently fetching some awesome features and some cool designs. Keep an eye out for updates!
---
+++ app/src/app/app.ts
@@ -0,0 +1,23 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {
+  protected readonly title = signal('app');
+}
