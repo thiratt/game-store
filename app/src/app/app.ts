a7c0bdbe0fb975423978d06b6f00d368b7378174
thiratt <thiratcha7@gmail.com>
2025-09-30 11:37:44 +0700

    Remove Angular Material dependencies and related styles

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -1,16 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
-
-interface Food {
-  value: string;
-  viewValue: string;
-}
 
 @Component({
   selector: 'app-root',
