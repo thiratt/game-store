bf004a1487c90e99814898c65376e3734956fc63
thiratt <thiratcha7@gmail.com>
2025-10-02 05:55:27 +0700

    feat: create Index page with routing and component integration

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Index } from './pages/index';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Index,
+  },
+];
---
+++ app/src/app/pages/index/index.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Navbar } from "../../components/navbar/navbar";
+
+@Component({
+  selector: 'app-index',
+  imports: [Navbar],
+  templateUrl: './index.html',
+  styleUrl: './index.scss'
+})
+export class Index {
+
+}
