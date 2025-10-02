69cc0cc0d20a962a1efdceacb3ebbddbb3d2d730
thiratt <thiratcha7@gmail.com>
2025-10-02 07:55:06 +0700

    feat: add signup route to authentication module

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Index } from './pages/index';
 import { Login } from './pages/auth/login/login';
 import { Auth } from './components/layout/auth/auth';
+import { Signup } from './pages/auth/signup/signup';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
       {
         path: 'login',
         component: Login,
+      },
+      {
+        path: 'signup',
+        component: Signup,
       },
     ],
   },
