191238cba2d6aa5744d9d227bbc09a3068d87806
thiratt <thiratcha7@gmail.com>
2025-10-02 07:20:11 +0700

    feat: add authentication routes for login component

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
 import { Index } from './pages/index';
+import { Login } from './pages/auth/login/login';
+import { Auth } from './components/layout/auth/auth';
 
 export const routes: Routes = [
   {
     path: '',
     component: Index,
+  },
+  {
+    path: 'auth',
+    component: Auth,
+    children: [
+      {
+        path: 'login',
+        component: Login,
+      },
+    ],
   },
 ];
