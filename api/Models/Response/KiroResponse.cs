6cad9160f46931b9a55be31a325a3b444ab170da
thiratt <thiratcha7@gmail.com>
2025-10-04 03:20:49 +0700

    fix: change Message property to use default empty string initialization

--- api/Models/Response/KiroResponse.cs
+++ api/Models/Response/KiroResponse.cs
@@ -3,6 +3,6 @@ namespace api.Models.Response;
 public class KiroResponse
 {
     public bool Success { get; set; }
-    public required string Message { get; set; }
+    public string Message { get; set; } = string.Empty;
     public object? Data { get; set; }
 }
