d99b474bfcc50099edcfd9563ea5f3543b7ebf16
thiratt <thiratcha7@gmail.com>
2025-10-04 13:18:20 +0700

    fix: update Message property in KiroResponse to be nullable

--- api/Models/Response/KiroResponse.cs
+++ api/Models/Response/KiroResponse.cs
@@ -3,6 +3,6 @@ namespace api.Models.Response;
 public class KiroResponse
 {
     public bool Success { get; set; }
-    public string Message { get; set; } = string.Empty;
+    public string? Message { get; set; }
     public object? Data { get; set; }
 }
