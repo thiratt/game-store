1f5affa9f712c619f634355bd7cdab5c02fe6a87
thiratt <thiratcha7@gmail.com>
2025-10-05 13:56:51 +0700

    feat: add detailed response to hello endpoint

--- api/Controllers/HelloController.cs
+++ api/Controllers/HelloController.cs
@@ -2,19 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models.Response;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
-    [Route("[controller]")]
+    [Route("/")]
     [ApiController]
     public class HelloController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<string>> GetStrings()
+        public ActionResult<KiroResponse> GetStrings()
         {
-            await Task.Delay(1);
-            return Ok("Hello World!");
+            var response = new KiroResponse
+            {
+                Success = true,
+                Message = "Hello, World!",
+                Data = new
+                {
+                    Ip = HttpContext.Connection.RemoteIpAddress?.ToString(),
+                }
+            };
+            return Ok(response);
         }
     }
 }
