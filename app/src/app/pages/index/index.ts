851e15e1416b6f754761e7a93197586de763b1f2
thiratt <thiratcha7@gmail.com>
2025-10-11 14:29:13 +0700

    feat: add Games component to index page

--- app/src/app/pages/index/index.ts
+++ app/src/app/pages/index/index.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { UserNavigationBar } from '../../components/navbar/user/user-navigation-bar';
 import { Hero } from '../../components/landing/hero/hero';
 import { TopSeller } from '../../components/landing/top-seller/top-seller';
+import { Games } from "../../components/landing/games/games";
 
 @Component({
   selector: 'app-index',
-  imports: [UserNavigationBar, Hero, TopSeller],
+  imports: [UserNavigationBar, Hero, TopSeller, Games],
   templateUrl: './index.html',
   styleUrl: './index.scss',
 })
