76acc990de77abae61fd78eea0aff4c0c0fea5a1
thiratt <thiratcha7@gmail.com>
2025-10-11 08:53:11 +0700

    feat: add TopSeller component to index page

--- app/src/app/pages/index/index.ts
+++ app/src/app/pages/index/index.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UserNavigationBar } from '../../components/navbar/user/user-navigation-bar';
 import { Hero } from '../../components/landing/hero/hero';
+import { TopSeller } from '../../components/landing/top-seller/top-seller';
 
 @Component({
   selector: 'app-index',
-  imports: [UserNavigationBar, Hero],
+  imports: [UserNavigationBar, Hero, TopSeller],
   templateUrl: './index.html',
   styleUrl: './index.scss',
 })
