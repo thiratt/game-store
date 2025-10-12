0f67b4270589a932b512d0f804207bfa882fa196
thiratt <thiratcha7@gmail.com>
2025-10-13 04:34:36 +0700

    feat: add Transaction component with user transaction details and history display

---
+++ app/src/app/pages/dashboard/transaction/transaction.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { DecimalPipe, DatePipe } from '@angular/common';
+
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import { UserService } from '../../../services/user.service';
+import { UserTransactionDetail } from '../../../interfaces/user.interface';
+
+@Component({
+  selector: 'app-transaction',
+  imports: [DecimalPipe, DatePipe, CardModule, ButtonModule, TableModule],
+  templateUrl: './transaction.html',
+  styleUrl: './transaction.scss',
+})
+export class Transaction {
+  users: UserTransactionDetail[] = [];
+  userSelection: UserTransactionDetail | null = null;
+
+  constructor(private readonly userService: UserService) {
+    this.userService.getAllUsers().subscribe((users) => {
+      this.users = users;
+      this.userSelection = this.users[0];
+    });
+  }
+
+  get endpoint(): string {
+    return this.userService.endpoint;
+  }
+}
