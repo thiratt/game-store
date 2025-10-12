36cde34f2873f9af593fe6eed674f25a28b56cc9
thiratt <thiratcha7@gmail.com>
2025-10-13 04:34:08 +0700

    feat: add UserTransactionDetail and TransactionHistory interfaces for transaction management

--- app/src/app/interfaces/user.interface.ts
+++ app/src/app/interfaces/user.interface.ts
@@ -6,3 +6,20 @@ export interface User {
   profileImage?: string;
   walletBalance: number;
 }
+
+export interface UserTransactionDetail {
+  id: string;
+  email: string;
+  username: string;
+  walletBalance: number;
+  role: string;
+  profileImage?: string;
+  transactionHistories: TransactionHistory[];
+}
+
+export interface TransactionHistory {
+  id: string;
+  amount: number;
+  type: string;
+  transactionDate: Date;
+}
